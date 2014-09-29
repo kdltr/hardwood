e325545999097f0e717cffb7782ace467b83c81d
Adrien Ramos <kooda@upyum.com>
2014-09-29 07:14:35 +0200

    Implement the generic (and quite huge) message processing function `msg-rcv`. Implement `recv`, `??` and `?` using `rcv-msg`.

--- hardwood.scm
+++ hardwood.scm
@@ -1,7 +1,22 @@
-(use srfi-1 srfi-18)
+(use srfi-1 srfi-18 matchable)
 
-(define no-default (list 'no-default))
-(define timeout-condition (make-property-condition 'hardwood-timeout))
+(define timeout-condition
+  (make-composite-condition (make-property-condition 'exn
+                                                     'message "Timeout while receiving")
+                            (make-property-condition 'hardwood)
+                            (make-property-condition 'timeout)))
+(define (hardwood-timeout-condition? exn)
+  (and ((condition-predicate 'hardwood) exn)
+       ((condition-predicate 'timeout) exn)))
+(define (no-default)
+  (signal timeout-condition))
+
+; TODO
+; Send a patch for matchable so that it signals an exception instead of using
+; (error)
+(define (no-match-condition? exn)
+  (string=? "no matching pattern"
+            (get-condition-property exn 'exn 'message "")))
 
 (define mailbox-head
   (make-parameter '()))
@@ -32,25 +47,68 @@
         (mutex-unlock! lock)
         #t))))
 
-(define (?? pred? #!optional (timeout #f) (default no-default))
-  (let ((timeout (if (or (time? timeout) (not timeout))
+(define (rcv-msg proc timeout timeout-proc)
+  (let ((timeout (if (or (not timeout) (time? timeout))
                    timeout
                    (seconds->time
                      (+ (time->seconds (current-time))
                         timeout)))))
-    (receive (head tail) (break pred? (mailbox-head))
-      (if (null? tail)
-        (if (wait-for-messages timeout)
-          (?? pred? timeout default)
-          (if (eqv? default no-default)
-            (signal timeout-condition)
-            default))
-        (begin
-          (mailbox-head (append head (cdr tail)))
-          (car tail))))))
+    (let loop ((prev '()))
+      (let* ((head (mailbox-head))
+             (empty? (null? head))
+             (msg (or empty? (car head))))
+        (if empty?
+          (if (wait-for-messages timeout)
+            (loop prev)
+            (begin
+              (mailbox-head (append (reverse prev)
+                                    head))
+              (timeout-proc)))
+          (begin
+            (mailbox-head (cdr head))
+            (receive (ok? . vals)
+                     (handle-exceptions exn
+                                        (begin
+                                          (mailbox-head (append (reverse prev)
+                                                                (cons msg
+                                                                      (mailbox-head))))
+                                          (abort exn))
+                       (proc msg))
+                     (if ok?
+                       (begin
+                         (mailbox-head (append (reverse prev)
+                                               (mailbox-head)))
+                         (apply values vals))
+                       (loop (cons msg prev))))))))))
+
+(define (?? pred? #!optional (timeout #f) (default no-default))
+  (rcv-msg (lambda (m) (values (pred? m) m))
+           timeout
+           (if (eqv? default no-default)
+             no-default
+             (lambda () default))))
 
 (define (? #!optional (timeout #f) (default no-default))
   (?? (lambda x #t) timeout default))
+
+(define-syntax recv
+  (ir-macro-transformer
+    (lambda (expr inject compare)
+      (let* ((clauses (inject (cdr expr)))
+             (timeout-clause (assoc 'after clauses))
+             (timeout (and timeout-clause (cadr timeout-clause)))
+             (timeout-proc (and timeout-clause (caddr timeout-clause)))
+             (clauses (remove (lambda (e) (eqv? (car e) 'after)) clauses)))
+        `(rcv-msg (lambda (m)
+                    (handle-exceptions exn
+                                       (if (no-match-condition? exn)
+                                         #f
+                                         (abort exn))
+                      (apply values (cons #t
+                                          (receive (match m
+                                                          ,@clauses))))))
+                  ,timeout
+                  (lambda () ,timeout-proc))))))
 
 (define (! pid msg)
   (let* ((specific (thread-specific pid))
